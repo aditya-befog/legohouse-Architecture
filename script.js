1a58d4399cc9741aa8b756fc21c79fa9e5bac9c4
vinay2003 <vinaysharma31681@gmail.com>
2024-10-26 09:21:03 +0530

    all section is completed

--- script.js
+++ script.js
@@ -33,11 +33,36 @@ document.addEventListener("scroll", () => {
     imageCenter.style.transform = `translateY(${centerImageOffset}%)`;
   
   });
+  // gsap.registerPlugin(ScrollTrigger);
+
+  // if (window.innerWidth <= 768) {
+  //   startMobileScroll();
+  // }
+
   gsap.registerPlugin(ScrollTrigger);
 
-  if (window.innerWidth <= 768) {
-    startMobileScroll();
-  }
+    const workInfoItems = document.querySelectorAll('.work-photo-item');
+    workInfoItems.forEach((item, index) => {
+      item.style.zIndex = workInfoItems.length - index;
+    });
+
+    gsap.set(".work-photo-item", {
+      clipPath: "inset(0px 0px 0px 0px)"
+    });
+
+    const animation = gsap.to('.work-photo-item:not(:last-child)', {
+      clipPath: "inset(0px 0px 100% 0px)",
+      stagger: 0.5,
+      ease: "none"
+    });
+
+    ScrollTrigger.create({
+      trigger: '.work',
+      start: 'top top',
+      end: 'bottom bottom',
+      animation: animation,
+      scrub: 1
+    });
   
 const cardsWrapper = gsap.utils.toArray(".cards_item");
 const cardEl = gsap.utils.toArray(".cards_el");
