db9c80a16d5391241faa62505d2b35b8ae68082c
vinay2003 <vinaysharma31681@gmail.com>
2024-10-28 17:08:56 +0530

    stories page done

---
+++ Stories/stories.js
@@ -0,0 +1,27 @@
+const menuToggle = document.getElementById('menuToggle');
+    const menu = document.getElementById('menu');
+    const hamburger = document.getElementById('hamburger');
+    const close = document.getElementById('close');
+
+    menuToggle.addEventListener('click', () => {
+      menu.classList.toggle('active');
+      hamburger.style.display = hamburger.style.display === 'none' ? 'block' : 'none';
+      close.style.display = close.style.display === 'none' ? 'block' : 'none';
+    });
+gsap.registerPlugin(ScrollTrigger);
+let cards = gsap.utils.toArray(".cards_item");
+let mm = gsap.matchMedia();
+mm.add("(min-width: 767px)", () => {
+    gsap.to(cards, {
+        xPercent: (i) => -i * 100 + (i > 0 ? 4.5 * i : 0),
+        duration: 0.5,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".section-card",
+            pin: true,
+            markers: true,
+            scrub: 0.1,
+            end: "+=3000 bottom",
+        },
+    });
+});
--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-const menuToggle = document.getElementById('menuToggle');
-    const menu = document.getElementById('menu');
-    const hamburger = document.getElementById('hamburger');
-    const close = document.getElementById('close');
+const menuToggle = document.getElementById("menuToggle");
+const menu = document.getElementById("menu");
+const hamburger = document.getElementById("hamburger");
+const close = document.getElementById("close");
 
-    menuToggle.addEventListener('click', () => {
-      menu.classList.toggle('active');
-      hamburger.style.display = hamburger.style.display === 'none' ? 'block' : 'none';
-      close.style.display = close.style.display === 'none' ? 'block' : 'none';
-    });
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.style.display =
+    hamburger.style.display === "none" ? "block" : "none";
+  close.style.display = close.style.display === "none" ? "block" : "none";
+});
 
 document.addEventListener("scroll", () => {
-    const scrollPosition = window.pageYOffset; 
-  
-    const mainSection = document.querySelector(".main");
-    const imageLeft = document.querySelector(".image-left");
-    const imageCenter = document.querySelector(".image-center");
-    const imageRight = document.querySelector(".image-right");
-  
-    if (scrollPosition > 100) {
-      mainSection.classList.add("shrink");
-    } else {
-      mainSection.classList.remove("shrink");
-    }
+  const scrollPosition = window.pageYOffset;
 
-    const leftImageOffset = Math.max(10 - scrollPosition * 0.5, -250);
-    const rightImageOffset = Math.max(10 - scrollPosition * 0.5, -250);
-    
-    const centerImageOffset = Math.min(-270 + scrollPosition * 0.5, 0);
- 
-    imageLeft.style.transform = `translateY(${leftImageOffset}%)`;
-    imageRight.style.transform = `translateY(${rightImageOffset}%)`;
-    imageCenter.style.transform = `translateY(${centerImageOffset}%)`;
-  
-  });
-  // gsap.registerPlugin(ScrollTrigger);
+  const mainSection = document.querySelector(".main");
+  const imageLeft = document.querySelector(".image-left");
+  const imageCenter = document.querySelector(".image-center");
+  const imageRight = document.querySelector(".image-right");
 
-  // if (window.innerWidth <= 768) {
-  //   startMobileScroll();
-  // }
+  if (scrollPosition > 100) {
+    mainSection.classList.add("shrink");
+  } else {
+    mainSection.classList.remove("shrink");
+  }
 
-  let currentIndex = 0;
-const items = document.querySelectorAll('.work-photo-item');
+  const leftImageOffset = Math.max(10 - scrollPosition * 0.5, -250);
+  const rightImageOffset = Math.max(10 - scrollPosition * 0.5, -250);
+
+  const centerImageOffset = Math.min(-270 + scrollPosition * 0.5, 0);
+
+  imageLeft.style.transform = `translateY(${leftImageOffset}%)`;
+  imageRight.style.transform = `translateY(${rightImageOffset}%)`;
+  imageCenter.style.transform = `translateY(${centerImageOffset}%)`;
+});
+// gsap.registerPlugin(ScrollTrigger);
+
+// if (window.innerWidth <= 768) {
+//   startMobileScroll();
+// }
+
+let currentIndex = 0;
+const items = document.querySelectorAll(".work-photo-item");
 const totalItems = items.length;
 
 function showNextImage() {
-  items[currentIndex].classList.remove('active');
+  items[currentIndex].classList.remove("active");
   currentIndex = (currentIndex + 1) % totalItems;
-  items[currentIndex].classList.add('active');
+  items[currentIndex].classList.add("active");
 }
 
 setInterval(showNextImage, 3000); // Change image every 3 seconds
@@ -55,29 +55,28 @@ const cardsWrapper = gsap.utils.toArray(".cards_item");
 const cardEl = gsap.utils.toArray(".cards_el");
 
 cardsWrapper.forEach((e, i) => {
-    const card = cardEl[i];
-    let scale = 1;
-    let rotate = 0;
+  const card = cardEl[i];
+  let scale = 1;
+  let rotate = 0;
 
-    if (i !== cardEl.length - 1) {
-        scale = 0.9 + 0.025 * i;
-        rotate = -10;
-    }
+  if (i !== cardEl.length - 1) {
+    scale = 0.9 + 0.025 * i;
+    rotate = -10;
+  }
 
-    gsap.to(card, {
-        scale: scale,
-        rotationX: rotate,
-        transformOrigin: "top center",
-        ease: "none",
-        scrollTrigger: {
-            trigger: e,
-            start: `top ${70 + 40 * i}px`,
-            end: "bottom +=650px",
-            endTrigger: ".end-anim",
-            pin: e,
-            pinSpacing: false,
-            scrub: true
-        }
-    });
+  gsap.to(card, {
+    scale: scale,
+    rotationX: rotate,
+    transformOrigin: "top center",
+    ease: "none",
+    scrollTrigger: {
+      trigger: e,
+      start: `top ${70 + 40 * i}px`,
+      end: "bottom +=650px",
+      endTrigger: ".end-anim",
+      pin: e,
+      pinSpacing: false,
+      scrub: true,
+    },
+  });
 });
-
